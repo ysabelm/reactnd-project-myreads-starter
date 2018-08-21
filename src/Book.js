b135116ce9c68009641fab7cfa0a92b64a6b5d19
ysabelm <ysabelmonfort@gmail.com>
2018-08-21 19:31:05 +0200

    Adds Book component

---
+++ src/Book.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import BookShelfChanger from './BookShelfChanger'
+
+class Book extends React.Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
+
+  render() {
+    const { book } = this.props
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.thumbnail})`
+              }}
+            />
+            <BookShelfChanger
+
+            />
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+        </div>
+      </li>
+
+    )
+  }
+}
+
+export default Book
