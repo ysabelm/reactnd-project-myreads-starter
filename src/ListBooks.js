eaa8223d2872ec473ebfbabb35deb208fa2a5834
ysabelm <ysabelmonfort@gmail.com>
2018-08-23 12:17:19 +0200

    Adds shelfCategory and bookShelves var, modifies BookShelf component props

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -6,26 +6,36 @@ import PropTypes from 'prop-types'
 class ListBooks extends React.Component {
   static propTypes = {
     books: PropTypes.array.isRequired,
-    //changeShelf: PropTypes.func.isRequired
+    changeShelf: PropTypes.func.isRequired
   }
 
   state = {}
 
   render() {
-    const { books, shelves } = this.props
+    const { books, changeShelf } = this.props
+    const shelfCategory = [
+      { category: 'currentlyReading', title: 'Currently Reading' },
+      { category: 'wantToRead', title: 'Want to Read' },
+      { category: 'read', title: 'Read' }]
+
+
 
     return (
       <div className="list-books-content">
-        {shelves.map((shelf) => (
-          <li key={books.shelf}>
-            <BookShelf
-              books={books}
-              shelfTitle={shelf.title}
-              shelves={shelves}
-            //changeShelf={changeShelf}
-            />
-          </li>
-        ))}
+        {shelfCategory.map((shelf, index) => {
+          const bookShelves = books.filter(book => book.shelf === shelf.category)
+          return (
+            <div className="bookshelf" key={index}>
+              <h2 className="bookshelf-title">{shelf.title}</h2>
+              <div className="bookshelf-books">
+                <BookShelf
+                  books={bookShelves}
+                  changeShelf={changeShelf}
+                />
+              </div>
+            </div>
+          )
+        })}
       </div>
     )
   }
